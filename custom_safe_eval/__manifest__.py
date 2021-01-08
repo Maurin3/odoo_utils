8a8381489a92401c3d1bc4899c1bbe498a938ca1
Maurin3 <maurin3_1994@msn.com>
2021-01-08 20:08:40 +0100

    [IMP] custom_error_logs : 1.0.2
    Add the pprint to safe_eval function of Odoo.
    Closes #1.

--- custom_safe_eval/__manifest__.py
+++ custom_safe_eval/__manifest__.py
@@ -1,12 +1,12 @@
 {
     'name': 'Custom Safe Eval',
-    'version': '1.0.1',
+    'version': '1.0.2',
     'category': 'Tools',
     'summary': 'Custom Safe Eval (Rewrite)',
     'description': """
 Custom Safe Eval
 ===================
-Module that rewrite the safe_eval to let (i)pdb, print function to be used in server actions, crons, automated actions, ...
+Module that rewrite the safe_eval to let (i)pdb, (p)print function to be used in server actions, crons, automated actions, ...
 """,
     'author': 'mau@odoo.com',
     'depends': [
--- custom_safe_eval/tools/safe_eval.py
+++ custom_safe_eval/tools/safe_eval.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from odoo.tools.safe_eval import _BUILTINS
+from pprint import pprint
+
 import pdb
 import ipdb
 
 _BUILTINS['print'] = print
+_BUILTINS['pprint'] = pprint
 _BUILTINS['pdb'] = pdb
 _BUILTINS['ipdb'] = ipdb
